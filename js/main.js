85e17cead0f4e780dc26d4b6ca06c4f53a808dd5
MattiaVolonterio <volonteriomattia@gmail.com>
2024-01-29 15:31:21 +0100

    feat: button to change title color

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@ createApp({
       titleInfo: {
         pageTitle: "Questo è il titolo della pagina",
         titleImgSrc: "./img/logo.svg",
+        titleColor: "",
       },
     };
+  },
+
+  methods: {
+    changeColor() {
+      this.titleInfo.titleColor =
+        this.titleInfo.titleColor == "" ? "text-danger" : "";
+    },
   },
 }).mount("#app");
